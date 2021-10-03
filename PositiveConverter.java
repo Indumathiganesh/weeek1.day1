3c85628344e553bfc10b593485397c43405c5379
Indumathiganesh <91772141+Indumathiganesh@users.noreply.github.com>
2021-10-03 14:16:14 +0530

    Add files via upload

---
+++ PositiveConverter.java
@@ -0,0 +1,14 @@
+package week1.day1.assignment;
+
+public class PositiveConverter {
+
+	public static void main(String[] args) {
+		int num1 = -20;
+		int num2 = 0;
+				if (num1 < 0) {
+					num2 = num1 * -1;
+				}
+System.out.println(num1 + " is converted to " + num2);
+				}
+	}
+
---
+++ PositiveOrNegative.java
@@ -0,0 +1,19 @@
+package week1.day1.assignment;
+
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		
+		int number  =10;
+		
+		if(number%2==0)
+		{
+			System.out.println("even number");
+		}
+		else
+		{
+			System.out.println("ODD number");
+		}
+	}
+
+}
